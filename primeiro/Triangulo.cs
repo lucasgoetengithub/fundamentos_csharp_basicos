cf7cf10965a90375eb269459b51eec1fe9d7c6f8
Lucas <lucasgoetengithub@gmail.com>
2022-06-11 17:04:05 -0300

    Metodo area triangulo

--- primeiro/Triangulo.cs
+++ primeiro/Triangulo.cs
@@ -1,8 +1,15 @@
-﻿namespace Primeiro {
+﻿using System;
+
+namespace Primeiro {
     class Triangulo {
 
         public Double A;
         public Double B;
         public Double C;
+
+        public double Area() {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
     }
 }
