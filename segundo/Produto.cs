a62f0dd6421b80f6f0fb10a98dcb20d9009a504e
Lucas <lucasgoetengithub@gmail.com>
2022-06-11 19:37:29 -0300

    ciraçã das classes prduto e primeiro metodo

---
+++ segundo/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace segundo {
+    class Produto {
+        public string Nome;
+        public double preco;
+        public int quantidade;
+
+        public double ValorTotalEmEstoque() {
+            return preco * quantidade;
+        }
+
+
+    }
+}
---
+++ segundo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace segundo {
+    class Program {
+        static void Main(string[] args) {
+            Produto produto = new Produto();
+            Console.WriteLine("Entre com as informações do produto");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preco: ");
+            produto.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            produto.preco = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do produto: ");
+        }
+    }
+}
